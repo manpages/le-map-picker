281e095f2d4f745b54047904c7e33087dda82dad
Radivarig <reslav.hollos@gmail.com>
2016-03-22 08:54:34 +0100

    (add) display items

--- src/WheelPicker.jsx
+++ src/WheelPicker.jsx
@@ -4,6 +4,16 @@ var WheelPicker = React.createClass({
   render() {
     return (
       <div>
+        {
+          this.props.items.map((x, i)=>{
+            return (
+              <div key={i}>
+                <div>{x.name}</div>
+                <img src={'/src/images/400px-' +x.src +'.jpg'} />
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
--- src/WheelPickerViewer.jsx
+++ src/WheelPickerViewer.jsx
@@ -3,13 +3,44 @@ var WheelPicker = require('./WheelPicker.jsx')
 
 var WheelPickerViewer = React.createClass({
   render() {
+    var items = [
+      {
+        src: 'Dusk_Towers',
+        name: 'Dusk Towers'
+      },
+      {
+        src: 'Orbital_Shipyard',
+        name: 'Orbital Shipyard'
+      },
+      {
+        src: 'Prion_Terraces',
+        name: 'Prion Terraces'
+      },
+      {
+        src: 'Ulrena',
+        name: 'Ulrena'
+      },
+      {
+        src: 'Central_Protocol',
+        name: 'Central Protocol'
+      },
+      {
+        src: 'Lerilak_Crest',
+        name: 'Lerilak Crest'
+      },
+      {
+        src: 'Ruins_of_Seras',
+        name: 'Ruins of Seras'
+      }
+    ]
+
     return (
       <div>
         <div>
           Le Wheel Picker
         </div>
         <div>
-          <WheelPicker />
+          <WheelPicker items={items} />
         </div>
       </div>
     )
