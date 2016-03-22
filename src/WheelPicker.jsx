a1de59b319c56a2e528ad5d9ba52049859a1c0f5
Radivarig <reslav.hollos@gmail.com>
2016-03-22 11:17:38 +0100

    (add) motion

--- src/WheelPicker.jsx
+++ src/WheelPicker.jsx
@@ -1,15 +1,50 @@
 var React = require('react')
 
 var WheelPicker = React.createClass({
+  getInitialState() {
+    return {
+      speed: 5,
+      top: 0,
+      items: [],
+    }
+  },
+
+  movePosition() {
+    var top = this.state.top +this.state.speed
+    this.setState({top})
+  },
+
+  componentDidMount() {
+    this.setItems(this.props.items)
+    setInterval(this.movePosition, 10)
+  },
+
+  setItems(propsItems) {
+    var items =
+      propsItems.map((x, i)=>{
+        return (
+          <div key={i}>
+            <div>{x.name}</div>
+            <img src={x.src} />
+          </div>
+        )
+      })
+    this.setState({items})
+  },
+
   render() {
+    var elemH = 400
     return (
-      <div>
+      <div style={{position: 'relative', height: elemH, width: elemH, overflow: 'hidden'}}>
         {
-          this.props.items.map((x, i)=>{
+          this.state.items.map((x, i)=>{
+            var s = {
+              position: 'absolute',
+              top: (this.state.top +(i+1)*elemH) %(this.state.items.length *elemH)  -elemH
+            }
             return (
-              <div key={i}>
-                <div>{x.name}</div>
-                <img src={x.src} />
+              <div key={i} style={s}>
+                {x}
               </div>
             )
           })
